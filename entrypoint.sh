cc1c046f8f9c108b5b6e6b316cd830581eb42dd7
Linus Juhlin <linus.juhlin@protonmail.com>
2022-05-26 10:21:23 +0900

    ZEN-9725 Add multiple runs and format output properly

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,4 +20,12 @@ else
     echo "composer.json not found in repo, skipping Composer installation"
 fi
 
-/composer/vendor/bin/ecs check $1 --fix
+OUTPUT=$(/composer/vendor/bin/ecs check $1 --fix)
+OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix)" # Second run just in case
+OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix)" # Third run just in case
+OUTPUT="${OUTPUT//'%'/'%25'}"
+OUTPUT="${OUTPUT//$'\n'/'%0A'}"
+OUTPUT="${OUTPUT//$'\r'/'%0D'}"
+
+echo "OUTPUT: $OUTPUT"
+echo "::set-output name=ecs_output::$OUTPUT"
