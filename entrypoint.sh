816983eadc1236f2f22ffe8bd38d020a3a6c5cf7
Linus Juhlin <linus.juhlin@protonmail.com>
2022-06-09 10:17:43 +0900

    ZEN-9725 If we clear cache we might actually get more fixes in

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,9 +20,10 @@ else
     echo "composer.json not found in repo, skipping Composer installation"
 fi
 
-OUTPUT=$(/composer/vendor/bin/ecs check $1 --fix)
-OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix)" # Second run just in case
-OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix)" # Third run just in case
+# Run three times to make sure we get everything
+OUTPUT=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache)
+OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix --clear-cache)"
+OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix --clear-cache)"
 OUTPUT="${OUTPUT//'%'/'%25'}"
 OUTPUT="${OUTPUT//$'\n'/'%0A'}"
 OUTPUT="${OUTPUT//$'\r'/'%0D'}"
