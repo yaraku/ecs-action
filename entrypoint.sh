9ea38b8eced26456ef89ef63a92eab8f83043c61
Linus Juhlin <linus.juhlin@protonmail.com>
2022-07-21 07:00:30 +0900

    ZEN-9786 Output json files and then remove them for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,13 @@ OUTPUT="${OUTPUT//'%'/'%25'}"
 OUTPUT="${OUTPUT//$'\n'/'%0A'}"
 OUTPUT="${OUTPUT//$'\r'/'%0D'}"
 
-echo "OUTPUT: $OUTPUT"
+echo "JSON FILES:"
+echo "ONE"
+cat json_one.json
+echo "TWO"
+cat json_two.json
+echo "THR"
+cat json_thr.json
+
+rm json_one.json json_two.json json_thr.json
 echo "::set-output name=ecs_output::$OUTPUT"
