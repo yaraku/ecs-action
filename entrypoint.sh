212f33fb9d7930344ad98f1b63d6074fbd1bb7ab
Linus Juhlin <linus.juhlin@protonmail.com>
2022-07-20 16:02:35 +0900

    ZEN-9786 Remove unneeded composer code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,6 @@
 #!/bin/sh -l
 set -e
 
-if test -f "composer.json"; then
-    IGNORE_PLATFORM_REQS=""
-    if [ "$CHECK_PLATFORM_REQUIREMENTS" = "false" ] || [ "$INPUT_COMPOSER_IGNORE_PLATFORM_REQS" = "true" ]; then
-        IGNORE_PLATFORM_REQS="--ignore-platform-reqs"
-    fi
-
-    NO_DEV="--no-dev"
-    if [ "$REQUIRE_DEV" = "true" ] || [ "$INPUT_COMPOSER_REQUIRE_DEV" = "true"  ]; then
-        NO_DEV=""
-    fi
-
-    COMPOSER_COMMAND="composer install --no-scripts --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
-    echo "::group::$COMPOSER_COMMAND"
-    $COMPOSER_COMMAND
-    echo "::endgroup::"
-else
-    echo "composer.json not found in repo, skipping Composer installation"
-fi
-
 # Run three times to make sure we get everything
 OUTPUT=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache)
 OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix --clear-cache)"
