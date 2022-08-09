b10203c876dc9e1a831665b76968cfbb2d8032e1
Linus Yamashita <linus.yamashita@protonmail.com>
2022-08-09 09:46:20 +0900

    ZEN-9964 Reduced runs from 3 to 1 and only use 1 json file as ouput

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,36 +2,14 @@
 set -e
 
 # Run three times to make sure we get everything
-echo $(/composer/vendor/bin/ecs check $1 --clear-cache --output-format=json > one.json)
-echo $(/composer/vendor/bin/ecs check $1 --clear-cache --output-format=json > two.json)
-echo $(/composer/vendor/bin/ecs check $1 --clear-cache --output-format=json > thr.json)
+echo $(/composer/vendor/bin/ecs check $1 --clear-cache --output-format=json > output.json)
 
-FILES=""
-
-if [ ! "$(cat one.json | jq '.totals|add')" = "0" ]; then
-    FILES="one.json"
-fi
-
-if [ ! "$(cat two.json | jq '.totals|add')" = "0" ]; then
-    FILES="$FILES two.json"
-fi
-
-if [ ! "$(cat thr.json | jq '.totals|add')" = "0" ]; then
-    FILES="$FILES thr.json"
-fi
-
-cat $FILES
-
-# Merge json
-if [ ! "$FILES" = "" ]; then
-    OUTPUT=$(jq -s 'def f(x;y): reduce y[] as $item (x; reduce ($item | keys_unsorted[]) as $key (.; $item[$key] as $val | ($val | type) as $type | .[$key] = if ($type == "object") then f({};[if .[$key] == null then {} else .[$key] end, $val]) elif ($type == "array") then (.[$key] + $val | unique) else $val end)); f({};.)' $FILES)
-else
-    OUTPUT="$(cat one.json)"
-fi
+cat output.json
+OUTPUT="$(cat output.json)"
 
 OUTPUT="${OUTPUT//'%'/'%25'}"
 OUTPUT="${OUTPUT//$'\n'/'%0A'}"
 OUTPUT="${OUTPUT//$'\r'/'%0D'}"
 
-rm one.json two.json thr.json
+rm output.json
 echo "::set-output name=ecs_output::$OUTPUT"
