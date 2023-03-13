7fade9b5552ed72ced106e6487feb5773fea1033
Linus Yamashita <linus@yaraku.com>
2023-03-13 16:04:50 +0900

    ZEN-10274 Adjust path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 set -e
 
-OUTPUT=$(./vendor/bin/pint $1 --test --preset=psr12 -v --format=json)
+OUTPUT=$(/composer/vendor/bin/pint $1 --test --preset=psr12 -v --format=json)
 
 echo "OUTPUT: $OUTPUT"
 echo "ecs_output=$OUTPUT" >> "$GITHUB_OUTPUT"
